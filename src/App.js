8785653ee611736d86c7ad60cb40697d0872b209
JJONGHyun-JEON <wjswhdgus153@gmail.com>
2023-11-15 16:53:04 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const 한재현병신스타일 = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textDecoration: 'none',
+    fontSize: '80px',
+    color: '#000',
+    width: '100%',
+    height: '100vh',
+  }
+  return (
+    <div className="App">
+      <a style={한재현병신스타일}>한재현 장애인 고아년의 PPT 시작합니다.</a>
+    </div>
+  );
+}
+
+export default App;
